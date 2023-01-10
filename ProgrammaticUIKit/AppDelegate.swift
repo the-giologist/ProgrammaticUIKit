f7dac512aa87bf092b1aaece2c915a9879808204
Gio Peña <thegiologist@gmail.com>
2023-01-09 21:57:38 -0600

    Removed Main.storyboard references and setup delegate entry points to use programmatic view controllers.

--- ProgrammaticUIKit/AppDelegate.swift
+++ ProgrammaticUIKit/AppDelegate.swift
@@ -9,11 +9,22 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 13.0, *) {
+            //Use SceneDelegate instead as entry point.
+        } else {
+            let rootNavController = UINavigationController()
+            let dashboardViewController = DashboardViewController()
+            rootNavController.viewControllers = [dashboardViewController]
+            
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = rootNavController
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
---
+++ ProgrammaticUIKit/DashboardViewController.swift
@@ -0,0 +1,107 @@
+//
+//  DashboardViewController.swift
+//  ProgrammaticUIKit
+//
+//  Created by Gio Peña on 1/9/23.
+//
+
+import UIKit
+
+class DashboardViewController: UITableViewController {
+    var tableSections: [SettingsTableSection] = [
+        SettingsTableSection(title: "Server Settings", cells: [SettingsTableSection.CellData(type: .button, label: "idk")
+                                                          ])
+//    SettingsTableSection(title: "FTP", cells: <#T##[SettingsTableSection.SettingsTableCellData]#>),
+//    SettingsTableSection(title: "Printer", cells: <#T##[SettingsTableSection.SettingsTableCellData]#>),
+//    SettingsTableSection(title: "btn-refreshSettings", cells: <#T##[SettingsTableSection.SettingsTableCellData]#>),
+//    SettingsTableSection(title: "Btn-TouchID", cells: <#T##[SettingsTableSection.SettingsTableCellData]#>)
+    ]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewController()
+        setupTableView()
+    }
+    
+    
+    struct SettingsTableSection {
+        var title: String
+        var cells: [CellData]
+        
+        init(title: String, cells: [CellData]) {
+            self.title = title
+            self.cells = cells
+        }
+        
+        
+        
+        struct CellData {
+            var type: CellType
+            var label: String
+//            var placeholder: String
+            enum CellType {
+                case button, navigationbutton, textField
+            }
+            
+            init(type: CellType, label: String) {
+                self.type = type
+                self.label = label
+            }
+        }
+    }
+    
+    
+    
+
+    
+    private func setupTableView() {
+        tableView = UITableView(frame: tableView.frame, style: .insetGrouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "\(DashboardViewController.self)")
+        tableView.dataSource = self
+        tableView.delegate = self
+        //tableView.rowHeight = 44
+    }
+    
+    
+    private func setupViewController() {
+        self.title = "\(DashboardViewController.self)"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gearshape.2.fill"),
+                                                                 style: .done,
+                                                                 target: self,
+                                                                 action: #selector(trailingButtonAction))
+    }
+    
+    
+    @objc func trailingButtonAction() {
+        print("tbd...")
+    }
+
+}
+
+
+
+
+
+
+
+
+//MARK: - UITableView
+extension DashboardViewController {
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return tableSections[section].title
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableSections.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(DashboardViewController.self)", for: indexPath)
+        cell.textLabel?.text = tableSections[indexPath.section].cells[indexPath.row].label
+        return cell
+    }
+    
+}
